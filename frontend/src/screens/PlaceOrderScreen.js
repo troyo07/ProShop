d7ba93531e6179892d1489071243a366fbae8304
troyo07 <troydicko@gmail.com>
2020-12-01 10:56:42 +0000

    PlaceOrderScreen complete

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -8,6 +8,22 @@ import { Link } from "react-router-dom"
 const PlaceOrderScreen = () => {
   const cart = useSelector((state) => state.cart);
 
+  //Caluculate Prices
+  const addDecimals = (num) => {
+      return (Math.round(num * 100) / 100).toFixed(2)
+    }
+
+  cart.itemsPrice = addDecimals(cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0))
+
+  cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100)
+
+  cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)))
+
+  cart.totalPrice = (
+    Number(cart.itemsPrice) + Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2)
+  
   const placeOrderHandler = () => {
   console.log("order")
 }

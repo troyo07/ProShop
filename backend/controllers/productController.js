ed7453d8a06fe5c4bbed9450b4c18d28463682f1
troyo07 <troydicko@gmail.com>
2020-12-07 11:03:49 +0000

    admin Product/delete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -26,4 +26,20 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById }
+// @ description  Deletesingle product
+// @route DELETE /api/products/:id
+// @access Private/admin
+
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove()
+    res.json({ message: "product removed"})
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById, deleteProduct }
